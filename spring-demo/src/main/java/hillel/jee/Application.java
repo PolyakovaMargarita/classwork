df2811107044f477fb9045f1d77d238c7315520f
Dmitriy Butakov <dmitry404@gmail.com>
2019-05-24 23:15:04 +0300

    adds simple spring project

---
+++ spring-demo/src/main/java/hillel/jee/Application.java
@@ -0,0 +1,20 @@
+package hillel.jee;
+
+import java.util.Arrays;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("/application-context.xml");
+
+    System.out.println("-----------------");
+    System.out.println("[DEBUG] Registered beans:");
+    Arrays.stream(context.getBeanDefinitionNames())
+        .map(beanName -> "- " + beanName)
+        .forEach(System.out::println);
+
+    System.out.println("-----------------");
+  }
+}
